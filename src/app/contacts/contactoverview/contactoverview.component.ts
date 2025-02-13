206cfc56077705ac650a5b33a65a225d14507a04
SinanDmrr <sinan-demir@hotmail.com>
2025-02-13 16:25:29 +0100

    Contacts.service.ts so that we can now subscribe to the id of our clicked elements and retrieve them in other components. e.g. contactlist click and then retrieve them in the contactoverview component.

--- src/app/contacts/contactoverview/contactoverview.component.ts
+++ src/app/contacts/contactoverview/contactoverview.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContactsService } from '../../firebase-services/contacts.service';
+import { Icontacts } from '../../interfaces/icontacts';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contactoverview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contactoverview.component.html',
-  styleUrl: './contactoverview.component.scss'
+  styleUrl: './contactoverview.component.scss',
 })
 export class ContactoverviewComponent {
-
+  public contacts = inject(ContactsService);
+  selectedContact: Icontacts | null = null; // Variable für den ausgewählten Kontakt
+  constructor() {
+    this.contacts;
+  }
+  ngOnInit(): void {
+    // Abonniere das selectedContactId$-Observable
+    this.contacts.selectedContactId$.subscribe((id) => {
+      if (id) {
+        // Finde den Kontakt in der contactlist anhand der ID
+        this.selectedContact =
+          this.contacts.contactlist.find((contact) => contact.id === id) ||
+          null;
+      } else {
+        this.selectedContact = null; // Setze selectedContact auf null, wenn keine ID vorhanden ist
+      }
+    });
+  }
 }
