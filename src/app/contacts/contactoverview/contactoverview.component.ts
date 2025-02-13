435592a0229f12f297580e65b9a01317f05b5cb0
Theokleses <osahonschmolze222@gmail.com>
2025-02-13 16:34:06 +0100

    Added basic-structure for contactoverview

--- src/app/contacts/contactoverview/contactoverview.component.ts
+++ src/app/contacts/contactoverview/contactoverview.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContactsService } from '../../firebase-services/contacts.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contactoverview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contactoverview.component.html',
-  styleUrl: './contactoverview.component.scss'
+  styleUrl: './contactoverview.component.scss',
 })
 export class ContactoverviewComponent {
-
+  public contacts = inject(ContactsService);
+  constructor() {
+    this.contacts;
+  }
+  
 }
