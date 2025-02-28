79345d0936b2448a6c4dbdc23c89143a3946cbbe
Theokleses <osahonschmolze222@gmail.com>
2025-02-28 14:29:43 +0100

    Responsiv animation added for log-in component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 
@@ -10,12 +10,41 @@ import { AppComponent } from '../app.component';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  constructor(private router: Router, private appComponent: AppComponent) {}
+export class LoginComponent implements AfterViewInit {
+  constructor(
+    private router: Router,
+    private appComponent: AppComponent,
+    private renderer: Renderer2
+  ) {}
+
+  ngAfterViewInit(): void {
+    const logoElement = document.querySelector('.logo');
+    const resetAnimation = () => {
+      this.renderer.removeAttribute(logoElement, 'style');
+      this.renderer.removeClass(logoElement, 'animate-logo');
+      setTimeout(() => {
+        this.renderer.addClass(logoElement, 'animate-logo');
+        setTimeout(() => {
+          this.renderer.removeClass(logoElement, 'animate-logo');
+        }, 2000);
+      }, 10);
+    };
+
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+    const handleMediaChange = (e: MediaQueryListEvent | MediaQueryList) => {
+      if (e.matches) {
+        resetAnimation();
+      } else {
+        this.renderer.removeClass(logoElement, 'animate-logo');
+      }
+    };
+    handleMediaChange(mediaQuery);
+    mediaQuery.addEventListener('change', handleMediaChange);
+  }
 
   navigateToSummary() {
     this.appComponent.isLoggedIn = true;
-    this.router.navigate(['/summary']); 
+    this.router.navigate(['/summary']);
   }
 
   navigateToSignup() {
