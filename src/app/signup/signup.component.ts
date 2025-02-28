a557830817624d94a706dccb60607cc9827a12ea
Theokleses <osahonschmolze222@gmail.com>
2025-02-28 18:32:56 +0100

    Sign-up button disabled if checkbox is not clicked and style changes

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,8 +9,25 @@ import { Router } from '@angular/router';
   styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  isPrivacyAccepted: boolean = false;
+
   constructor(private router: Router) {}
+
   navigateToLogin() {
     this.router.navigate(['/login']); 
   }
+
+  onCheckboxChange(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    this.isPrivacyAccepted = checkbox.checked; // Aktualisiert den Status
+    console.log('Checkbox status:', this.isPrivacyAccepted); // Debugging
+  }
+
+  signUp() {
+    if (this.isPrivacyAccepted) {
+      console.log('Sign up clicked!');
+      // Hier kannst du deine Signup-Logik einfügen, z. B. this.router.navigate(['/somewhere']);
+    }
+  }
+
 }
